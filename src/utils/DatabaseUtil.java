695a01cf0ba2c496097f5d2043ad90c5ffcf917a
Quayyum-a <quayyumariyo@gmail.com>
2025-05-18 14:29:11 +0100

    Refactor DatabaseUtil to load database configuration from properties file

--- src/utils/DatabaseUtil.java
+++ src/utils/DatabaseUtil.java
@@ -1,15 +1,37 @@
 package utils;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DatabaseUtil {
-    private static final String URL = "jdbc:mysql://localhost:3306/student_course_db";
-    private static final String USER = "root"; // Replace with your MySQL username
-    private static final String PASSWORD = "Mavixess"; // Replace with your MySQL password
+    private static Properties properties = new Properties();
+    private static boolean loaded = false;
+
+    private static void loadProperties() {
+        if (!loaded) {
+            try (InputStream input = DatabaseUtil.class.getClassLoader().getResourceAsStream("utils/config.properties")) {
+                if (input == null) {
+                    System.err.println("Unable to find config.properties");
+                    return;
+                }
+                properties.load(input);
+                loaded = true;
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
 
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+        loadProperties();
+        String url = properties.getProperty("db.url", "jdbc:mysql://localhost:3306/student_course_db");
+        String user = properties.getProperty("db.user", "root");
+        String password = properties.getProperty("db.password", "");
+
+        return DriverManager.getConnection(url, user, password);
     }
 }
