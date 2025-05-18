7539bcb9194983a7cc35ddee9bd132ba5d773bfc
Quayyum-a <quayyumariyo@gmail.com>
2025-05-18 12:42:38 +0100

    Add User model class with basic attributes

---
+++ src/data/model/User.java
@@ -0,0 +1,10 @@
+package data.model;
+
+public class User {
+    private String fullName;
+    private int id;
+    private String email;
+    private String password;
+    private String role;
+
+}
