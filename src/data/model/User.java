dc63ec2264230715d9135122decee813a2b6a92f
Quayyum-a <quayyumariyo@gmail.com>
2025-05-18 13:05:59 +0100

    Add User class with constructor and getter/setter methods

--- src/data/model/User.java
+++ src/data/model/User.java
@@ -7,4 +7,51 @@ public class User {
     private String password;
     private String role;
 
+    public User(String fullName, int id, String email, String password, String role) {
+        this.fullName = fullName;
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
